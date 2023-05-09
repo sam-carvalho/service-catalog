271844d66cc8681134bb4e4027ec1f67a11a20ba
Samantha Carvalho <samantha.desouzacarvalho@move.com>
2023-05-08 21:04:21 -0700

    refactor: update header and search bar components

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -3,10 +3,19 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 import React from 'react';
 
-describe('Header', () => {
-  test('should render the header and a search bar', () => {
-    render(<Header />);
-    expect(screen.getByTestId('header')).toBeInTheDocument();
-    expect(screen.getByTestId('search-bar')).toBeInTheDocument();
+const mockHandleOpenMenu = jest.fn();
+
+describe("Header", () => {
+  test("should render the header and a search bar", () => {
+    render(
+      <Header
+        menuWidth={240}
+        isMenuOpen={false}
+        handleOpenMenu={mockHandleOpenMenu}
+        isMobile={false}
+      />
+    );
+    expect(screen.getByTestId("header")).toBeInTheDocument();
+    expect(screen.getByTestId("search-bar")).toBeInTheDocument();
   });
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import SearchBar from './SearchBar';
+import { IconButton, Toolbar, Box } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import SearchBar from "./SearchBar";
+import StyledAppBar from "./StyledAppBar";
 
-const drawerWidth = 240;
+interface HeaderProps {
+  isMenuOpen: boolean;
+  isMobile: boolean;
+  menuWidth: number;
+  handleOpenMenu: () => void;
+}
 
-const SearchContainer = styled(Toolbar)({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-});
-
-const Header = () => {
-    return (
-        <AppBar
-            position="fixed"
-            sx={{
-                width: { sm: `calc(100% - ${drawerWidth}px)` },
-                ml: { sm: `${drawerWidth}px` },
-                bgcolor: '#fff',
-                color: '#000',
-                boxShadow: 'none',
-                borderBottom: '1.5px solid #ddd'
-            }}
-            data-testid="header"
+const Header = ({
+  isMenuOpen,
+  handleOpenMenu,
+  menuWidth,
+  isMobile,
+}: HeaderProps) => {
+  return (
+    <StyledAppBar isMenuOpen={isMenuOpen} menuWidth={menuWidth}>
+      <Toolbar sx={{ bgcolor: "#fff", color: "#000", display: "flex" }}>
+        <IconButton
+          color="inherit"
+          aria-label="open menu"
+          onClick={handleOpenMenu}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(isMenuOpen && { display: "none" }),
+          }}
         >
-            <SearchContainer>
-                <SearchBar />
-            </SearchContainer>
-        </AppBar>
-    );
+          <Menu />
+        </IconButton>
+        <Box sx={{ flexGrow: 1 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: isMobile ? "flex-start" : "center",
+            }}
+          >
+            <SearchBar />
+          </Box>
+        </Box>
+      </Toolbar>
+    </StyledAppBar>
+  );
 };
 
 export default Header;
