e02cf7e2369e014bc12b1cf96135f147ac2e4eea
Samantha Carvalho <samantha.desouzacarvalho@move.com>
2023-06-11 22:53:17 -0700

    feat: add search feature

---
+++ src/context/search/SearchProvider.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { SearchContext } from "./SearchContext";
+
+interface SearchProviderProps {
+  children: React.ReactNode;
+}
+
+export const SearchProvider: React.FC<SearchProviderProps> = ({ children }) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
